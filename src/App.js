0e17518b9ad8291d483f6558738f83c8ecd788fe
Katja P <katjapesonen1@gmail.com>
2022-06-14 15:06:38 +0300

    Work in progress...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import allContacts from "./contacts.json"
+import { useState } from 'react';
+
+const firstFive = allContacts.slice(0,5)
+
 
 function App() {
+
+  const [contacts, setContacts] = useState(firstFive)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <h2>Iron Contacts</h2>
+      
+      { contacts.map((celeb, index) => {
+        return (
+          <div key={celeb.id}>
+          <img src={ celeb.pictureUrl } alt="celeb photo" />
+          <p> { celeb.name } </p>
+          <p> { celeb.popularity } </p>
+          </div>
+        )
+      })}
+      
     </div>
   );
 }
+ 
+
 
 export default App;
+
